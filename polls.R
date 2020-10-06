d65927cf593792485c52da0b9ea56fa37d62ed2e
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-10-06 11:41:48 -0700

    hw5

--- polls.R
+++ polls.R
@@ -11,3 +11,18 @@ mean(is.na(polls$methodology))
 # Not enough columns with missing data :(
 # Maybe next assignment.
 lapply(polls, function(x) mean(is.na(x)))
+
+
+approval = read.csv("~/data/approval_polllist.csv")
+
+# Nope
+lapply(approval, function(x) mean(is.na(x)))
+
+
+cap = read.csv("~/data/covid_approval_polls.csv")
+
+lapply(cap, function(x) mean(is.na(x)))
+# a handful of approve, disapprove, and sample_size missing
+# Nope
+
+lapply(cap, function(x) sum(is.na(x)))
