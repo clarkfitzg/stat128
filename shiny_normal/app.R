949ae58556f93c1adf7fa11816c48f154fee1158
Clark Fitzgerald <clarkfitzg@gmail.com>
2020-11-17 16:15:19 -0800

    send to students

--- shiny_normal/app.R
+++ shiny_normal/app.R
@@ -6,6 +6,7 @@ ui <- fluidPage(
   , numericInput("mean", label = "mean", value = 0)
   , numericInput("sd", label = "standard deviation", value = 1, min = 0.1)
   , numericInput("n", label = "number of random samples in histogram", value = 100, min = 5)
+  , numericInput("breaks", label = "number of breaks in histogram", value = 10, min = 5)
   , plotOutput("distplot")
 )
 
@@ -19,12 +20,12 @@ server <- function(input, output) {
     main = sprintf("Mean %g and standard deviation %g", input$mean, input$sd)
 
     samples = rnorm(nsamples, mean = imean, sd = isd)
-    h = hist(samples, freq = FALSE)
+    h = hist(samples, breaks = input$breaks, plot = FALSE)
 
     x = seq(from = min(samples), to = max(samples), length.out = 1000)
     y = dnorm(x, mean = imean, sd = isd)
 
-    plot(h, ylim = c(0, max(y, h$density)), freq = FALSE)
+    plot(h, freq = FALSE, xlab = "", main = "", ylim = c(0, max(y, h$density)))
     lines(x, y, col = "blue", lwd = 2)
   })
   
